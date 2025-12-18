0db04d4f204459dc4718a5bfc556ec2f6ff0cb39
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:48:07 +0700

    Fungsi mennyimpan data siswa

--- app/src/main/java/com/asyiraaf/questapi_079/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/viewmodel/EntryViewModel.kt
@@ -7,21 +7,35 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.asyiraaf.questapi_079.modeldata.DetailSiswa
 import com.asyiraaf.questapi_079.modeldata.UIStateSiswa
+import com.asyiraaf.questapi_079.modeldata.toSiswa
+import retrofit2.Response
 
 class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    /* Fungsi untuk memvalidasi input */
+    // Fungsi untuk memvalidasi input
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-    // fungsi untuk menangani saat ada perubahan pada text input
+    // Fungsi untuk menangani saat ada perubahan pada text input
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    // Fungsi untuk menyimpan data siswa
+    suspend fun addSiswa() {
+        if (validasiInput()){
+            val sip: Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa.toSiswa())
+            if (sip.isSuccessful){
+                println("Sukses bro : ${sip.message()}")
+            }else{
+                println("Gagal bro : ${sip.errorBody()}")
+            }
+        }
     }
 }
