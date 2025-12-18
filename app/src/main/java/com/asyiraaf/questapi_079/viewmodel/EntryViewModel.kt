717803e1328d4312aae92a5688c6ee4b6b2f6241
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:38:53 +0700

    Validasi input form

---
+++ app/src/main/java/com/asyiraaf/questapi_079/viewmodel/EntryViewModel.kt
@@ -0,0 +1,21 @@
+package com.asyiraaf.questapi_079.viewmodel
+
+import RepositoryDataSiswa
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.asyiraaf.questapi_079.modeldata.DetailSiswa
+import com.asyiraaf.questapi_079.modeldata.UIStateSiswa
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+}
