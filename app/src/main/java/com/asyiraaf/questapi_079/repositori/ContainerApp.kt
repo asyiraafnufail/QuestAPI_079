0b7c77351ccc962d1da398580c7a7f59de9cfc72
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:19:36 +0700

    Konfigurasi Retrofit dan OkHttp client

--- app/src/main/java/com/asyiraaf/questapi_079/repositori/ContainerApp.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/repositori/ContainerApp.kt
@@ -1,8 +1,38 @@
 package com.asyiraaf.questapi_079.repositori
 
 import RepositoryDataSiswa
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 
 interface ContainerApp{
     val repositoryDataSiswa : RepositoryDataSiswa
+}
+
+class DefaultContainerrApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/questapi/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory("application/json".toMediaType())
+        )
+        .client(klien)
+        .build()
 }
 
