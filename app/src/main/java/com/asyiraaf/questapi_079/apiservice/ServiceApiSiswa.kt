6bbd1ab237fb20dc62313bafd357e41da7e0b3fc
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:04:22 +0700

    Mengirim data siswa ke server

--- app/src/main/java/com/asyiraaf/questapi_079/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/apiservice/ServiceApiSiswa.kt
@@ -1,10 +1,15 @@
 package com.asyiraaf.questapi_079.apiservice
 
 import com.asyiraaf.questapi_079.modeldata.DataSiswa
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
 
