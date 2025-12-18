4c6e906cd6f7eb06849864e76686acd42c2ada5d
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 14:52:29 +0700

    menambahkan DestinasiHome.kt

---
+++ app/src/main/java/com/asyiraaf/questapi_079/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.asyiraaf.questapi_079.uicontroller.route
+
+import com.asyiraaf.questapi_079.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
