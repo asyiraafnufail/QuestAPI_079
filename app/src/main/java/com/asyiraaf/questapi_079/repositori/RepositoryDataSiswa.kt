0c45271bde6b28c282667eeb7348db01b905ed60
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:22:33 +0700

    Implementasi repository yang menggunakan API jaringan

--- app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
@@ -1,8 +1,15 @@
 import com.asyiraaf.questapi_079.apiservice.ServiceApiSiswa
 import com.asyiraaf.questapi_079.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa{
     suspend fun getSiswa(): List<DataSiswa>
     suspend fun  postDatasiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa {
+    override suspend fun getSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDatasiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
