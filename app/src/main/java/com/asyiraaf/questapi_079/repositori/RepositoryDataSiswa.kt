aeef4d846d1208cdf9b36e82f04f74caf7de3bf8
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:06:18 +0700

    Fetch data siswa

--- app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
@@ -1,4 +1,7 @@
 package com.asyiraaf.questapi_079.repositori
 
+import com.asyiraaf.questapi_079.modeldata.DataSiswa
+
 class RepositoryDataSiswa {
+    suspend fun getDatasiswa(): List<DataSiswa> {
 }
