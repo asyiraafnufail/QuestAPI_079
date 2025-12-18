db778c34ae75e97682f9a6d87b98cb33ea6ef4a5
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-18 12:02:05 +0700

    API service untuk mengambil data siswa

--- app/src/main/java/com/asyiraaf/questapi_079/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,10 @@
 package com.asyiraaf.questapi_079.apiservice
 
+import com.asyiraaf.questapi_079.modeldata.DataSiswa
+import retrofit2.http.GET
+
 interface ServiceApiSiswa {
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
 }
+
