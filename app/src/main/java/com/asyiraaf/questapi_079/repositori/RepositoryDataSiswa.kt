e9c44ffd11ca08ffd564b8316039c6bd5279c6f9
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 14:14:18 +0700

    // Update RepositoryDataSiswa.kt

--- app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/repositori/RepositoryDataSiswa.kt
@@ -2,14 +2,37 @@ import com.asyiraaf.questapi_079.apiservice.ServiceApiSiswa
 import com.asyiraaf.questapi_079.modeldata.DataSiswa
 import retrofit2.Response
 
-interface RepositoryDataSiswa{
+interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
-    suspend fun  postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
-): RepositoryDataSiswa {
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+) : RepositoryDataSiswa {
+
+    override suspend fun getDataSiswa(): List<DataSiswa> =
+        serviceApiSiswa.getSiswa()
+
+    override suspend fun postDataSiswa(
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa =
+        serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(
+        id: Int,
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(
+        id: Int
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
 }
