41264b529f3772e191808a25c53326b12fbb92ed
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 16:55:14 +0700

    // Memperbaiki typo pada DestinasiEdit.kt

--- app/src/main/java/com/asyiraaf/questapi_079/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/uicontroller/route/DestinasiEdit.kt
@@ -2,9 +2,9 @@ package com.asyiraaf.questapi_079.uicontroller.route
 
 import com.asyiraaf.questapi_079.R
 
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "detail"
-    override val titleRes = R.string.detail_siswa
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "edit"
+    override val titleRes = R.string.edit_siswa
     const val itemIdArg = "itemId"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
