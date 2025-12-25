fcfb33bea9ac7f6a999310bbdb27ec9f1233f083
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 14:41:00 +0700

    // Add isi DestinasiEdit.kt

--- app/src/main/java/com/asyiraaf/questapi_079/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.asyiraaf.questapi_079.uicontroller.route
 
+import com.asyiraaf.questapi_079.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "itemId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
