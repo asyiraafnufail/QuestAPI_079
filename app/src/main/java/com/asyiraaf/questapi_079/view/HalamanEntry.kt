c2ec0d9d1256b4c177bea406382f8722765a1659
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 12:49:55 +0700

    // Composable untuk layar input data siswa dengan ViewModel dan navigasi kembali

--- app/src/main/java/com/asyiraaf/questapi_079/view/HalamanEntry.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/view/HalamanEntry.kt
@@ -1,2 +1,12 @@
 package com.asyiraaf.questapi_079.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
