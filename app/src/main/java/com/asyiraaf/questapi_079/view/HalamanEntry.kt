3eff8cf87f9a8a82644855e0005000bfaa956c75
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 12:50:51 +0700

    // Menyimpan CoroutineScope dan mengatur perilaku scroll TopAppBar

--- app/src/main/java/com/asyiraaf/questapi_079/view/HalamanEntry.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/view/HalamanEntry.kt
@@ -1,7 +1,13 @@
 package com.asyiraaf.questapi_079.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.asyiraaf.questapi_079.viewmodel.EntryViewModel
+import com.asyiraaf.questapi_079.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,4 +15,8 @@ fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-)
+){
+
+        val coroutineScope = rememberCoroutineScope()
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
