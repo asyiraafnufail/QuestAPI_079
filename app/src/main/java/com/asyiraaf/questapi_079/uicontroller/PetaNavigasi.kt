d0728a580afe1845970c67cf37625a2bc0a13c7e
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 16:56:27 +0700

    // Memperbaiki typo PetaNavigasi.kt

--- app/src/main/java/com/asyiraaf/questapi_079/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/asyiraaf/questapi_079/uicontroller/PetaNavigasi.kt
@@ -8,8 +8,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.asyiraaf.questapi_079.uicontroller.route.DestinasiDetail
+import com.asyiraaf.questapi_079.uicontroller.route.DestinasiEdit
 import com.asyiraaf.questapi_079.uicontroller.route.DestinasiEntry
 import com.asyiraaf.questapi_079.uicontroller.route.DestinasiHome
+import com.asyiraaf.questapi_079.view.DetailSiswaScreen
+import com.asyiraaf.questapi_079.view.EditSiswaScreen
 import com.asyiraaf.questapi_079.view.EntrySiswaScreen
 import com.asyiraaf.questapi_079.view.HomeScreen
 
@@ -18,7 +22,7 @@ fun DataSiswaApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
 ) {
-    HostNavigasi(navController = navController)
+    HostNavigasi(navController = navController, modifier = modifier)
 }
 
 @Composable
@@ -31,59 +35,56 @@ fun HostNavigasi(
         startDestination = DestinasiHome.route,
         modifier = modifier
     ) {
-
+        // Halaman Home
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                },
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = { id ->
                     navController.navigate("${DestinasiDetail.route}/$id")
                 }
             )
         }
 
+        // Halaman Entry
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(
                 navigateBack = {
-                    navController.navigate(DestinasiHome.route)
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
+                    }
                 }
             )
         }
 
+        // Halaman Detail
         composable(
             route = DestinasiDetail.routeWithArgs,
-            arguments = listOf(
-                navArgument(DestinasiDetail.itemIdArg) {
-                    type = NavType.IntType
-                }
-            )
+            arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+                type = NavType.IntType
+            })
         ) {
             DetailSiswaScreen(
                 navigateToEditItem = { id ->
                     navController.navigate("${DestinasiEdit.route}/$id")
                 },
-                navigateBack = {
-                    navController.navigate(DestinasiHome.route)
-                }
+                navigateBack = { navController.navigateUp() }
             )
         }
 
+        // Halaman Edit
         composable(
             route = DestinasiEdit.routeWithArgs,
-            arguments = listOf(
-                navArgument(DestinasiEdit.itemIdArg) {
-                    type = NavType.IntType
-                }
-            )
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
         ) {
             EditSiswaScreen(
                 navigateBack = {
-                    navController.navigate(DestinasiHome.route)
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) { inclusive = true }
+                    }
                 },
-                onNavigateUp = {
-                    navController.navigateUp()
-                }
+                onNavigateUp = { navController.navigateUp() }
             )
         }
     }
