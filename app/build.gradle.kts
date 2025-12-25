6190613f3bde07b8080cbffd5f895017ca2e3932
asyiraafnufail <asyiraaf4@gmail.com>
2025-12-25 13:10:09 +0700

    // Sync gradle menambahkan library baru

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.ui.text)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
